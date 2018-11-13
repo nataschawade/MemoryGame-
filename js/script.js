1939257c2bf845548732be6fe1fd21a0cbe83105
Natascha <natascha.wade@btinternet.com>
2018-11-13 23:37:23 +0200

    updates

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 //DECLARING VARIABLES
 var memoryCard = document.getElementsByClassName("memory-card");
 var memoryGame = document.getElementById("memory-game");
+var modal = document.getElementById('myModal');
+var modalContent = document.getElementById("modalContent")
 
 //SELECTING A CARD 
 info = {
     clickedItem: [],
     clickNumber: 0,
     correct: 0,
-    incorrect:0
+    incorrect: 0
 }
 
 function selectCard(e) {
@@ -24,66 +26,71 @@ function selectCard(e) {
 memoryGame.addEventListener("click", selectCard);
 
 //RANDOM SELECTOR
-var array = ["url('./images/cynthia.png')","url('./images/laura.png')","url('./images/gaby.png')", "url('./images/ilona.png')","url('./images/rebecca.png')","url('./images/emilie.png')"];
+var array = ["url('./images/cynthia.png')", "url('./images/laura.png')", "url('./images/gaby.png')", "url('./images/ilona.png')", "url('./images/rebecca.png')", "url('./images/emilie.png')"];
 function random(e) {
     var randomCard = array[Math.floor(Math.random() * array.length)];
     e.target.style.backgroundImage = randomCard;
-    e.target.style.transition= "all 1.0s  ";
-    e.target.style.transform="rotateY(180deg)";
+    e.target.style.transition = "all 1.0s  ";
+    e.target.style.transform = "rotateY(180deg)";
     info.clickedItem.push(randomCard);
 }
 
 
 //MATCH OR NO MATCH
 function decisionMaker() {
-    
-
     setTimeout(function () {
-
         if (info.clickedItem[0] === info.clickedItem[1]) {
-            
-            info.correct++ ; 
-
-            //MODAL  START
-            document.getElementById("modalText").innerHTML= "MATCH";
-            var modal = document.getElementById('myModal');
-            modal.style.display = "block";
-            // Get the <span> element that closes the modal
-            var span = document.getElementsByClassName("close")[0];
-            // When the user clicks on <span> (x), close the modal
-            span.onclick = function () {
-                modal.style.display = "none";
-            };
-            //MODAL END 
-            win();
+             correctAnswer();
         }
         else {
-            info.incorrect++ ;
-             
-             //MODAL  START
-             document.getElementById("modalText").innerHTML= "NO MATCH!";
-             var modal = document.getElementById('myModal');
-             modal.style.display = "block";
-             // Get the <span> element that closes the modal
-             var span = document.getElementsByClassName("close")[0];
-             // When the user clicks on <span> (x), close the modal
-             span.onclick = function () {
-                 modal.style.display = "none";
-             }
-             //MODAL END 
+            incorrectAnswer();
         };
-
+        //continue game
         info.clickNumber = 0;
         info.clickedItem = [];
-        // memoryGame.add("disable");;
         for (var i = 0; i < memoryCard.length; i++) {
             memoryCard[i].style.backgroundImage = "url('./images/itc.png')";
-            memoryCard[i].transition= "all 1.0s  ";
-            memoryCard[i].transform="rotateY(180deg)";
-            
+            memoryCard[i].transition = "all 1.0s  ";
+            memoryCard[i].transform = "rotateY(180deg)";
         }
     }, 1000);
 }
+
+// CORRECT ANSWER
+function correctAnswer() {
+    info.correct++;
+
+    //MODAL  START
+    document.getElementById("modalText").innerHTML = "MATCH!";
+    modal.style.display = "block";
+    modalContent.style.backgroundColor = "green";
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    };
+    //MODAL END 
+    win();
+}
+
+//INCORRECT ANSWER
+function incorrectAnswer() {
+    //MODAL  START
+    document.getElementById("modalText").innerHTML = "NO MATCH!";
+   
+    modal.style.display = "block";
+    modalContent.style.backgroundColor = "red";
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+    //MODAL END 
+    info.incorrect++;
+}
+
 
 //NEWGAME
 document.getElementById("newGame").addEventListener('click', function () {
@@ -94,11 +101,10 @@ document.getElementById("newGame").addEventListener('click', function () {
     }
 });
 
-// winner 
-
-function win (){
+// winner
+function win() {
     if (info.correct == 1) {
-alert("winner you have" + info.incorrect +"incorrect guesses")
+        alert("winner you have " + info.incorrect + " incorrect guesses")
     }
 }
 
@@ -106,7 +112,6 @@ alert("winner you have" + info.incorrect +"incorrect guesses")
 
 
 
-
-
 //need to make it so that once two have been clicked can no longer click
 //fliping images
+// when user wins the win appears and then the match!
