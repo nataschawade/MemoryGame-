671e8f3e0dc8e685c50f5600542c6f0b3e0c41a2
Natascha <natascha.wade@btinternet.com>
2018-11-18 08:13:55 +0200

    change theme long way

--- js/script.js
+++ js/script.js
@@ -5,7 +5,18 @@ var modal = document.getElementById('myModal');
 var modalContent = document.getElementById("modalContent");
 var modalText = document.getElementById("modalText");
 var newGame = document.getElementById("newGame");
+var body = document.getElementById("body");
+var changeTheme = document.getElementById("changeTheme");
+var backgroundImage1 = "url('./images/itc.png')";
 
+
+//EVENT LISTENERS
+memoryGame.addEventListener("click", assignCards);
+newGame.addEventListener("click", shuffle);
+newGame.addEventListener('click', newGameFunction);
+changeTheme.addEventListener("click", changeTheme1);
+
+//SELECTING A CARD 
 info = {
     clickedItem: [],
     clickNumber: 0,
@@ -26,6 +37,22 @@ var array = ["url('./images/cynthia.png')",
     "url('./images/rebecca.png')",
     "url('./images/emilie.png')",
 ];
+
+// var array2 = [
+//     "url('./images/ca1.jpg')",
+//     "url('./images/ca1.jpg')",
+//     "url('./images/ca2.jpg')",
+//     "url('./images/ca2.jpg')",
+//     "url('./images/ca10.jpg')",
+//     "url('./images/ca10.jpg')",
+//     "url('./images/ca4.jpg')",
+//     "url('./images/ca4.jpg')",
+//     "url('./images/ca5.jpg')",
+//     "url('./images/ca5.jpg')",
+//     "url('./images/ca8.jpg')",
+//     "url('./images/ca8.jpg')",
+// ];
+
 var cardId;
 var firstCard;
 var secondCard;
@@ -36,7 +63,8 @@ function assignCards(event) {
 
     if (event.target !== event.currentTarget) {
         info.clickNumber++;
-
+        event.target.style.backgroundImage= backgroundImage1;
+        event.target.style.boxShadow= "1px 1px 1px rgba(0,0,0,.3)";
         if (event.target === firstCard) return;
 
         cardId = event.target.getAttribute('id');
@@ -81,8 +109,8 @@ function correctAnswer() {
 function incorrectAnswer() {
     info.incorrect++;
     setTimeout(function () {
-        info.div[0].style.backgroundImage = "url('./images/itc.png')";
-        info.div[1].style.backgroundImage = "url('./images/itc.png')";
+        info.div[0].style.backgroundImage = backgroundImage1;
+        info.div[1].style.backgroundImage = backgroundImage1;
         info.clickedItem = [];
         info.div = [];
         memoryGame.addEventListener("click", assignCards);
@@ -98,7 +126,7 @@ function newGameFunction() {
     info.incorrect= 0
     info.div = [];
     for (var i = 0; i < memoryCard.length; i++) {
-        memoryCard[i].style.backgroundImage = "url('./images/itc.png')";
+        memoryCard[i].style.backgroundImage = backgroundImage1;
     }
 };
 
@@ -122,7 +150,7 @@ function win() {
     }, 2000);
 }
 
-//SHUFFLE 
+//SHUFFLE
 function shuffle() {
     for (var flips = 0; flips < array.length; flips++) {
         var i = Math.floor(Math.random() * array.length);
@@ -133,7 +161,52 @@ function shuffle() {
     }
 }
 
-//EVENT LISTENERS
-memoryGame.addEventListener("click", assignCards);
-newGame.addEventListener("click", shuffle);
-newGame.addEventListener('click', newGameFunction);
+// THEME 2 
+function changeTheme1() {
+    backgroundImage1 = "url('./images/backcard.jpg')";
+    for (var i = 0; i < memoryCard.length; i++) {
+        memoryCard[i].style.backgroundImage = backgroundImage1 ;
+    }
+    array.splice(0,12,  "url('./images/ca1.jpg')",
+    "url('./images/ca1.jpg')",
+    "url('./images/ca2.jpg')",
+    "url('./images/ca2.jpg')",
+    "url('./images/ca10.jpg')",
+    "url('./images/ca10.jpg')",
+    "url('./images/ca4.jpg')",
+    "url('./images/ca4.jpg')",
+    "url('./images/ca5.jpg')",
+    "url('./images/ca5.jpg')",
+    "url('./images/ca8.jpg')",
+    "url('./images/ca8.jpg')",);
+    body.style.backgroundImage = "url('./images/animalbackground.jpg')";
+    shuffle();
+    newGameFunction();
+    changeTheme.removeEventListener('click', changeTheme1);
+    changeTheme.addEventListener("click", changeTheme2);
+};
+
+//Theme2 
+function changeTheme2() {
+    array.splice(0,12, "url('./images/cynthia.png')",
+    "url('./images/laura.png')",
+    "url('./images/gaby.png')",
+    "url('./images/ilona.png')",
+    "url('./images/rebecca.png')",
+    "url('./images/emilie.png')",
+    "url('./images/cynthia.png')",
+    "url('./images/laura.png')",
+    "url('./images/gaby.png')",
+    "url('./images/ilona.png')",
+    "url('./images/rebecca.png')",
+    "url('./images/emilie.png')");
+    body.style.backgroundImage = "url('./images/background2.png')";
+    shuffle();
+    newGameFunction();
+    changeTheme.removeEventListener('click', changeTheme2);
+    changeTheme.addEventListener("click", changeTheme1);
+    backgroundImage1 = "url('./images/itc.png')";
+    for (var i = 0; i < memoryCard.length; i++) {
+        memoryCard[i].style.backgroundImage = backgroundImage1 ;
+    }
+};
